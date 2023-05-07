1ad9e690c7c9443e671968f2e3267981bab00add
Uroosa ahmed <uroosaahmed555@gmail.com>
2023-05-08 00:18:37 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+<div class="box">
+            <div class="right">
+               
+                <img class="cane" src="images/logo-2.jpg" alt="" srcset="">
+                <img src="https://www.pepsi.com/en-us/uploads/images/pzs-new-taste/new-improved-taste.svg" alt="new improved taste">
+
+            
+            </div>
+
+            <div class="left"></div>
+
+
+        </div>
